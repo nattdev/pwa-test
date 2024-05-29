3256a0b5a42b4f6274bb4352f3fbc6957a1ff5e8
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-28 20:44:08 -0500

    Implement vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+  manifest: {
+    name: 'Mi PWA',
+    short_name: 'PWA',
+    description: 'Una Progressive Web App de ejemplo',
+    theme_color: '#ffffff',
+    start_url: "/index.html",
+    icons: [
+      {
+        src: 'android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png'
+      }
+    ]
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA(pwaOptions)
+  ]
+});
